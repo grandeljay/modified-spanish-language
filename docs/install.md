17d7f677ac6f3ecf8f7b03e30549b31fe975d9f8
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-06 09:00:30 +0100

    Automatically perform a transfer on activation

--- docs/install.md
+++ docs/install.md
@@ -1,3 +1,3 @@
 # Installation
 
-After installing and activating this module, it is recommended to Transfer all of your default language's content to Spanish under `/admin/languages.php`.
+This is a all-in-one-solution. You just need to install and activate the modul, no further steps required.
--- src/admin/includes/modules/system/grandeljay_spanish_language.php
+++ src/admin/includes/modules/system/grandeljay_spanish_language.php
@@ -19,7 +19,6 @@ class grandeljay_spanish_language extends StdModule
     public function __construct()
     {
         parent::__construct();
-        }
     }
 
     public function display()
@@ -45,17 +44,166 @@ class grandeljay_spanish_language extends StdModule
 
         xtc_db_perform(TABLE_LANGUAGES, $sql_data_array);
 
-        $lID = xtc_db_insert_id();
+        $language_spanish_id = xtc_db_insert_id();
+
+        /**
+         * Copy content
+         */
+        $tables_query = xtc_db_query(
+            'SHOW TABLES;'
+        );
+
+        /** Get English language id */
+        $language_english_query = xtc_db_query(
+            sprintf(
+                'SELECT *
+                   FROM %s
+                  WHERE `code` = "%s"',
+                TABLE_LANGUAGES,
+                'en'
+            )
+        );
+        $language_english       = xtc_db_fetch_array($language_english_query);
+        $language_english_id    = $language_english['languages_id'];
+
+        /** Copy content */
+        while ($table = xtc_db_fetch_array($tables_query)) {
+            $table_name = reset($table);
+
+            if ('languages' === $table_name) {
+                continue;
+            }
+
+            $table_columns_query  = xtc_db_query(
+                sprintf(
+                    'SHOW COLUMNS FROM `%s`;',
+                    $table_name
+                )
+            );
+            $table_columns        = array();
+            $table_columns_ignore = array();
+
+            while ($table_columns_result = xtc_db_fetch_array($table_columns_query)) {
+                $table_column_name = $table_columns_result['Field'];
+
+                if (str_contains($table_columns_result['Extra'], 'auto_increment')) {
+                    $table_columns_ignore[] = $table_column_name;
+                }
+
+                $table_columns[] = $table_column_name;
+            };
+
+            $column_language_id = '';
+
+            if (in_array('language_id', $table_columns, true)) {
+                $column_language_id = 'language_id';
+            }
+
+            if (in_array('languages_id', $table_columns, true)) {
+                $column_language_id = 'languages_id';
+            }
+
+            if (!in_array($column_language_id, array('language_id', 'languages_id', true))) {
+                continue;
+            }
+
+            /** Delete potentially old/orphaned entries */
+            xtc_db_query(
+                sprintf(
+                    'DELETE FROM `%s`
+                           WHERE `%s` = %d;',
+                    $table_name,
+                    $column_language_id,
+                    $language_spanish_id
+                )
+            );
+
+            /**
+             * Insert copies
+             */
+            $copy_query = xtc_db_query(
+                sprintf(
+                    'SELECT *
+                       FROM `%s`
+                      WHERE `%s` = %d;',
+                    $table_name,
+                    $column_language_id,
+                    $language_english_id
+                )
+            );
+
+            while ($sql_data_array = xtc_db_fetch_array($copy_query)) {
+                foreach ($table_columns_ignore as $table_column_ignore) {
+                    unset($sql_data_array[$table_column_ignore]);
+                }
+
+                $sql_data_array[$column_language_id] = $language_spanish_id;
+
+                xtc_db_perform($table_name, $sql_data_array);
+            }
+
+            /**
+             * Insert correct translations
+             */
+
+            /** cookie_consent_categories */
+        }
     }
 
     public function remove()
     {
         parent::remove();
 
-        /** Remove Spanish language */
-        xtc_db_query(
-            'DELETE FROM `' . TABLE_LANGUAGES . '`
-                   WHERE `code` = "es"'
+        /** Get language information */
+        $language_spanish_query = xtc_db_query(
+            sprintf(
+                'SELECT *
+                   FROM `%s`
+                  WHERE `code` = "es"',
+                TABLE_LANGUAGES
+            )
         );
+        $language_spanish       = xtc_db_fetch_array($language_spanish_query);
+
+        /** Remove translations */
+        $language_id_columns = array(
+            'language_id',
+            'languages_id',
+        );
+
+        $tables_query = xtc_db_query(
+            'SHOW TABLES;'
+        );
+
+        while ($table = xtc_db_fetch_array($tables_query)) {
+            $table_name          = reset($table);
+            $table_columns_query = xtc_db_query(
+                sprintf('SHOW COLUMNS FROM %s;', $table_name)
+            );
+
+            $language_id_column = null;
+
+            while ($column = xtc_db_fetch_array($table_columns_query)) {
+                $column_name = $column['Field'];
+
+                if (in_array($column_name, $language_id_columns, true)) {
+                    $language_id_column = $column_name;
+
+                    break;
+                }
+            }
+
+            if ($language_id_column) {
+                $query = xtc_db_query(
+                    sprintf(
+                        'DELETE FROM %s
+                               WHERE %s = %d',
+                        $table_name,
+                        $language_id_column,
+                        $language_spanish['languages_id']
+                    )
+                );
+            }
+        }
     }
 }
